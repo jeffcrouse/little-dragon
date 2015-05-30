cc3f379a917272012d59eec32451b05885a9c68d
lars berg <laserstorm@gmail.com>
2015-05-30 15:02:20 -0500

    added composer app to sketches

---
+++ sketches/Composer/js/Interfaces/nxWrappers/MultiSliderWrapper.js
@@ -0,0 +1,128 @@
+// MultiSliderWrapper.js
+
+function MultiSliderWrapper( options )
+{
+	var scope = this;
+
+	var randf = THREE.Math.randFloat;
+	var mapLinear = THREE.Math.mapLinear;
+	var clamp = THREE.Math.clamp;
+	function lerp( a, b, k ){return a + (b-a) * k};
+	function smootherstep( x )
+	{
+	    return x*x*x*(x*(x*6 - 15) + 10);
+	}
+	var sin = Math.sin, cos = Math.cos;
+	var PI = Math.PI, HALF_PI = PI * .5, TWO_PI = PI * 2;
+	var v2 = function(x,y){	return new THREE.Vector3( x, y );}
+	var v3 = function(x,y,z){	return new THREE.Vector3( x, y, z );}
+
+	var controller = options.controller;
+	//colors
+	var c0 = options.c0 || new THREE.Color( 0xFFFFFF );
+	var c1 = options.c1 || new THREE.Color( 0x33FF88 );
+
+	var NUM_SLIDERS = controller.sliders;
+
+	var WIDTH = 1280; // controller.width;
+	var HEIGHT = 720; // controller.height;
+	var ASPECT_RATIO = WIDTH / HEIGHT;
+
+	var HALF_WIDTH = WIDTH * .5;
+	var HALF_HEIGHT = HEIGHT * .5;
+
+	var decay = .025;
+
+	var camera = options.camera || new THREE.OrthographicCamera( -HALF_WIDTH, HALF_WIDTH, HALF_HEIGHT, -HALF_HEIGHT, -1000, 1000 ); // 
+
+	var renderTarget = new THREE.WebGLRenderTarget( WIDTH * .25, HEIGHT * .25, {
+		minFilter: THREE.LinearFilter
+	} );
+
+	var scene = new THREE.Scene();
+
+	var autoClear = false;
+	
+	var clearingMesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( WIDTH, HEIGHT, 10, 10 ), new THREE.MeshBasicMaterial( {
+		transparent: true,
+		opacity: decay,
+		color: "black",
+		side: 2,
+		depthTest: false,
+		depthWrite: true,
+	} ) );
+
+	scene.add( clearingMesh );
+
+
+	//sliders
+	sliders = [];
+
+	var sliderMat = new THREE.MeshBasicMaterial( { color: "white" } );
+
+	var sliderGeometry = new THREE.BoxGeometry(1,1,.1, 2, 10 );
+
+	var xStep = WIDTH / NUM_SLIDERS;
+
+	for(var i=0; i<NUM_SLIDERS; i++)
+	{	
+		//	create a child mesh that is centered in the top half of the slider 
+		var m = new THREE.Mesh( sliderGeometry, sliderMat.clone() );
+		m.position.x = xStep * (i+.5) - HALF_WIDTH;
+		m.position.y = HEIGHT;
+		m.scale.x = xStep;
+		m.scale.y = HEIGHT;
+		m.scale.z = 100
+		scene.add( m );
+
+		// var rgb = v3(randf(-1, 1), randf(-1, 1), randf(-1, 1) ).normalize().multiplyScalar( 1.2 );
+		// m.material.color.setRGB( Math.abs(rgb.x), Math.abs(rgb.y), Math.abs(rgb.z) );
+
+		sliders[i] = m;
+	}
+
+
+	function draw( renderer )
+	{
+		renderer.render( scene, camera, renderTarget, autoClear );
+	}
+
+	function setSliderHieght( index, value )
+	{
+		if(sliders[index])
+		{
+			sliders[index].position.y = (value - 1) * HEIGHT;
+			// var k = cos( value * PI ) * -.5 + .5;
+			var k = value;//  1. - smootherstep( 1. - value );
+
+			sliders[index].material.color.copy( c0 ).lerp( c1, k );
+		}
+	}
+
+
+	scope.onHandleInput = function() {
+		// console.log( "scope.onHandleInput" );
+	}
+
+
+	function handleInput( data )
+	{
+		scope.onHandleInput( data );
+
+		for( var i in data.list ) {
+			setSliderHieght( i, data.list[i] );
+		}
+	}
+
+	return {
+		scene: scene,
+		camera: camera,
+		renderTarget: renderTarget,
+		draw: draw,
+		setSliderHieght: setSliderHieght,
+		c0: c0,
+		c1: c1,
+		handleInput: handleInput,
+		scope: scope
+	}
+}
---
+++ sketches/Composer/js/main.js
@@ -0,0 +1,133 @@
+//http://fbsisosurface:9876/?debug=true
+
+/**
+ * 
+ */
+
+
+var app;
+
+$(window).bind("load", function() {
+	var debug = getQuerystring('debug') == "true";
+	app = new APP( debug );
+});
+
+
+function APP( _debug )
+{
+
+	var debugImage = THREE.ImageUtils.loadTexture( "images/RCA_Indian_Head_test_pattern.JPG", null, function(t){
+		debugImage = t;
+		t.minFilter = THREE.LinearFilter;
+	} )
+
+	var ldInterface = BlendParticles({
+	  controller: {
+	  	width: window.innerWidth,
+	  	height: window.innerHeight,
+	  	canvasID: "textureReference",
+	  	texturePath: "images/rainbowGalaxy.jpeg"
+	  },
+	  spritePath: "textures/hexagon.png",
+	  numSpritesX: 120,
+	  spriteSize: 20,
+	  spriteBlending: 2,
+	  spriteOpacity: .34,
+	  c0: new THREE.Color( 0x34FFFF ),
+	  c1: new THREE.Color( 0xFF34FF ),
+	  useSideBars: false,
+	  showStats: false
+	});
+
+
+	var guiControls = {
+		particleSize: ldInterface.getParticleSize(),
+		particleOpacity: ldInterface.getParticleOpacity(),
+		spread: ldInterface.getSpread(),
+		offsetX: ldInterface.getOffsetX(),
+		offsetY: ldInterface.getOffsetY(),
+		count: ldInterface.getNumX(),
+		noiseScale: ldInterface.getNoiseScale()
+	}
+
+	var gui = new dat.GUI();
+
+	var guiContainer = $("<div>", {id: "GUIContainer"});
+	guiContainer.css({
+		position: "absolute",
+		left: "20px",
+		top: "20px",
+		pointerEvents: "auto"
+	});
+	$(ldInterface.container).append(guiContainer);
+
+
+	var input = $("<input type='file'></input>")
+	input.click( function(e){
+		input.val('')
+	}).css({
+		position: "absolute",
+		bottom: "-50px"
+	});
+
+	input.fileReaderJS( {
+		dragClass: "drag",
+		accept: false,
+		readAsMap: { 'text/*' : 'Text'},
+
+		// How to read any files not specified by readAsMap
+		readAsDefault: 'DataURL', //  'Text', // 'DataURL',
+		on: {
+			error: function(e, file) { console.log( "ERROR", e ); },
+			loadend: function( e, file ){
+				THREE.ImageUtils.loadTexture( e.target.result, null, function(t){
+					ldInterface.setImage( t );
+				})
+			},
+		}
+	} );
+
+	$(gui.domElement).append( input )
+
+	gui.add( guiControls, "particleSize", 1, 100 ).onChange( function( value ){
+		ldInterface.setParticleSize( value );
+	})
+	gui.add( guiControls, "particleOpacity", .001, 1.00 ).onChange( function( value ){
+		ldInterface.setParticleOpacity( value );
+	})
+	gui.add( guiControls, "spread", -1.0, 1.00 ).onChange( function( value ){
+		ldInterface.setSpread( value );
+	})
+
+	gui.add( guiControls, "noiseScale", .0, 2.00 ).onChange( function( value ){
+		ldInterface.setNoiseScale( value );
+	})
+
+	gui.add( guiControls, "offsetX", -250.0, 250.00 ).onChange( function( value ){
+		ldInterface.setOffsetX( value );
+	})
+
+	gui.add( guiControls, "offsetY", -250.0, 250.00 ).onChange( function( value ){
+		ldInterface.setOffsetY( value );
+	})
+
+	gui.add( guiControls, "count", 10, 250 ).onChange( function( value ){
+		ldInterface.setNumberOfParticles( parseInt( value ) );
+	})
+
+	// dat.GUI.toggleHide();
+	// 
+	guiContainer.append($(gui.domElement));
+}
+
+function getQuerystring(key, default_)
+{
+	if (default_==null) default_=""; 
+	key = key.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+	var regex = new RegExp("[\\?&]"+key+"=([^&#]*)");
+	var qs = regex.exec(window.location.href);
+	if(qs == null)
+		return default_;
+	else
+		return qs[1];
+}
