d81f9220f81bf4d9566d13575a79cff56b1158dd
lars berg <laserstorm@gmail.com>
2015-06-09 07:51:39 -0500

    first pass at projection composition

--- sketches/LDProjection/js/LDProjection.js
+++ sketches/LDProjection/js/LDProjection.js
@@ -13,16 +13,119 @@ $(window).bind("load", function() {
 
 });
 
+var LDProjectionKeyMaterial = function( params ) {
+
+	params = params || {};
+
+	var isLineShader = params.lineShader || false;
+
+	var matParams = {
+		transparent: true,
+		blending: params.blending || 2,
+		depthTest: params.depthTest || false,
+		depthWrite: params.depthWrite !== undefined ? params.depthWrite : false,
+		side: params.side || 0,// 0 = backFaceCull, 1 = frontFaceCull, 2 = doubleSided
+		linewidth: 1,
+
+		// TODO: if radius is staying at 1 lets remove it
+
+		uniforms: {
+			color: {type: 'c', value: new THREE.Color( params.color || "white" ) },
+			fade_color: {type: 'c', value: new THREE.Color( 0x0A0749 ) },
+			fade: {type: 'f', value: params.fade !== undefined ? params.fade : 0 },
+			opacity: {type: 'f', value: params.opacity || 1 },
+		},
+
+		vertexShader: [
+
+		'varying vec2 vUv;',
+
+		'void main() {',
+
+		'	vUv = uv * 2. - 1.;',
+
+		'	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+
+		'}'].join('\n'),
+
+		fragmentShader: [
+
+		'uniform float opacity;',
+
+		'uniform float fade;',
+
+		'uniform vec3 color;',
+
+		'uniform vec3 fade_color;',
+
+		'varying vec2 vUv;',
+
+		'float mapLinear( in float x, in float a1, in float a2, in float b1, in float b2 ) {',
+		'	return b1 + ( x - a1 ) * ( b2 - b1 ) / ( a2 - a1 );',
+		'}',
+
+		'float smootherstep( float x ){',
+		'    return x*x*x*(x*(x*6. - 15.) + 10.);',
+		'}',
+
+		'void main()',
+		'{',
+
+		'	vec3 c = mix( fade_color, color, (1. - smootherstep( abs( vUv.y ) ) * .9) * fade);',
+
+		// '	vec3 c = mix( fade_color, color * min( pow(sin( vUv.y * 3.14), 2.) + .25, 1.), fade);',
+
+		'	gl_FragColor = vec4( c, opacity );',
+
+		// '	float grad = 1.;',
+
+		// '	if(vUv.x < start)	grad = mapLinear( vUv.x, start-fadeDistance, start, 0., 1. );',
+
+		// '	else if(vUv.x > stop)	grad = mapLinear( vUv.x, stop, stop+fadeDistance, 1., 0. );',
+
+
+		// // '	float grad = vUv.x < start ? vUv.x / start : vUv.x > stop ? (1. - vUv.x) / (1. - stop) : 1. ;',
+
+		// '	grad = smootherstep( pow( max(0., grad), 1.5 ) );',
+
+		// '	gl_FragColor = vec4( vec3( grad ), 1. );',
+
+		'}'
+		].join('\n')
+
+	}
+	
+	THREE.ShaderMaterial.call( this, matParams );
+}
+
+LDProjectionKeyMaterial.prototype = Object.create( THREE.ShaderMaterial.prototype );
+
 
 
 function ProjectionVisuals( options ) {
 
 	options = options || {};
 
-	options.num = options.num || 1;
+	var lineLength = getQueryVariable("lineLength") || options.lineLength || 100;
 
-	var WIDTH = options.width || window.innerWidth;
-	var HEIGHT = options.height || window.innerHeight;
+	var lineWidth = getQueryVariable("lineWidth") || options.lineWidth || 4;	
+
+	var spacing = getQueryVariable("spacing") || options.spacing || 10;
+
+	var spriteRotation = getQueryVariable("rotation") || options.spriteRotation || Math.PI * 2;
+
+	var noiseScale = getQueryVariable("noiseScale") || options.noiseScale || .0075;
+
+	var noiseAmount = getQueryVariable("noiseAmount") || options.noiseAmount || 2;
+
+	var timeScale = getQueryVariable("timeScale") || options.timeScale || -1;
+
+	var numSpacers = 0;
+
+	var PROJECTOR_NUM = options.num || 1;
+
+	var WIDTH = 1280;// options.width || window.innerWidth;
+	var HEIGHT = 720;//options.height || window.innerHeight;
 	var ASPECT_RATIO = WIDTH / HEIGHT;
 
 	var HALF_WIDTH = WIDTH * .5;
@@ -57,15 +160,33 @@ function ProjectionVisuals( options ) {
 	}).appendTo( document.body );
 
 	//THREE
-	var scene, renderer, renderTarget, camera, group;
+	var scene, renderer, renderTarget, camera, group, clock = new THREE.Clock();
 
-	camera = new THREE.PerspectiveCamera( 60, ASPECT_RATIO, 1, 10000 );
-	camera.position.z = -100;
-	camera.lookAt( v3( 0, 0, 0 ) );
+	if( PROJECTOR_NUM === 1) {
 
+		camera = new THREE.OrthographicCamera( -HALF_WIDTH, HALF_WIDTH, HALF_HEIGHT, -HALF_HEIGHT, -1000, 1000 ); // 	
+
+	} else {
+
+		camera = new THREE.OrthographicCamera( HALF_WIDTH, -HALF_WIDTH, HALF_HEIGHT, -HALF_HEIGHT, -1000, 1000 ); // 
+
+	}
+	
+	// camera = new THREE.PerspectiveCamera( 60, ASPECT_RATIO, 1, 10000 );
+	// camera.position.z = -100;
+	// camera.lookAt( v3( 0, 0, 0 ) );
+
+	//render target
+	var rtScene = new THREE.Scene();
+	var rt = new THREE.WebGLRenderTarget( HALF_WIDTH, HALF_HEIGHT );
+	rt.minFilter = THREE.LinearFilter;
+
+	// group
 	scene = new THREE.Scene();
 
 	group = new THREE.Group();
+
+	scene.add( group );
 
 
 	var manager = new THREE.LoadingManager();
@@ -92,7 +213,8 @@ function ProjectionVisuals( options ) {
 	});
 
 	// OBJECTS
-	// 
+	var boxGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+	var baseColor = 0x111455;
 	// Keys
 	// keys1: Controls - multislider( 4 sliders )
 	// keys2: Interface_1 - keys( 7 keys )
@@ -101,7 +223,85 @@ function ProjectionVisuals( options ) {
 	// keys5: Interface_4 - keys( 7 keys )
 	// keys6: Tilt - tilt
 	
-	var 
+	var keyboards = {
+
+		Interface_1: {
+			color: 0xD5F7FA,
+			count: 7,
+			keys: [],
+			group: new THREE.Group()
+		},
+
+		Interface_2: {
+			color: 0xC1F4F9,
+			count: 7,
+			keys: [],
+			group: new THREE.Group()
+		},
+
+		Interface_3: {
+			color: 0xB0F2FA,
+			count: 7,
+			keys: [],
+			group: new THREE.Group()
+		},
+
+		Interface_4: {
+			color: 0x9CF1FA,
+			count: 7,
+			keys: [],
+			group: new THREE.Group()
+		},
+
+	}
+
+	console.log( 'keyboards', keyboards );
+	
+	var keysGroup = new THREE.Group();
+
+	group.add( keysGroup );
+
+	var key_index = 0;
+	for(var i in keyboards ) {
+
+		keysGroup.add( keyboards[i].group );
+
+		for(var j=0; j<keyboards[i].count; j++ ) {
+
+			var m = new THREE.Mesh( boxGeometry, new LDProjectionKeyMaterial( {
+				color: keyboards[i].color 
+			} ) );
+
+			m.fadeTween = new TWEEN.Tween( m.material.uniforms.fade )
+				.to( {value: 1}, randf( 30, 150 ) )
+				.repeat( 100 )
+				.delay( randf( 250, 1000) )
+				.yoyo( true )
+				.onUpdate( function( value ) {
+					this.scale.z = 1. + this.material.uniforms.fade.value;
+				}.bind( m ))
+				.start();
+
+			m.scale.x = .9;
+
+			m.position.x = key_index + .5;
+
+			keyboards[i].keys.push( m );
+
+			keyboards[i].group.add( m );
+
+			key_index++;
+		}
+
+	}
+
+	//	transform the keys as a whole
+	keysGroup.position.x -= HALF_WIDTH;
+	keysGroup.position.y -= HEIGHT / 3;
+
+	keysGroup.scale.x = WIDTH / key_index;
+	keysGroup.scale.y = HEIGHT * 2 / 3;
+	keysGroup.scale.z = HEIGHT * .5;
 
 	// Bass
 	// bass1: Controls - multislider( 4 sliders )
@@ -110,14 +310,173 @@ function ProjectionVisuals( options ) {
 	// bass4: Interface_3 - keys( 4 keys )
 	// bass5: Interface_4 - keys( 3 keys )
 	// bass6: Tilt - tilt
+	
+	var bass = {
+
+		Interface_1: {
+			color: 0x2BFCB7,
+			count: 4,
+			keys: [],
+			group: new THREE.Group()
+		},
+
+		Interface_2: {
+			color: 0x2BFECA,
+			count: 3,
+			keys: [],
+			group: new THREE.Group()
+		},
+
+		Interface_3: {
+			color: 0x2CFEDD,
+			count: 4,
+			keys: [],
+			group: new THREE.Group()
+		},
+
+		Interface_4: {
+			color: 0x2DFFFE,
+			count: 3,
+			keys: [],
+			group: new THREE.Group()
+		},
+	}
+
+	var bassGroup = new THREE.Group();
+	group.add( bassGroup );
+
+	bass_index = 0;
+	for(var i in bass) {
+
+		bassGroup.add( bass[i].group );
+
+		for(var j=0; j<bass[i].count; j++ ) {
+
+			var m = new THREE.Mesh( boxGeometry, new LDProjectionKeyMaterial( {
+				color: bass[i].color 
+			} ) );
+
+
+			m.fadeTween = new TWEEN.Tween( m.material.uniforms.fade )
+				.to( {value: 1}, randf( 100, 250 ) )
+				.repeat( 100 )
+				.delay( randf( 250, 1000) )
+				.yoyo( true )
+				.onUpdate( function( value ) {
+					this.scale.z = 1. + this.material.uniforms.fade.value;
+				}.bind( m ))
+				.start();
+
+			m.scale.x = .9;
+
+			m.position.x = bass_index + .5;
+
+			bass[i].keys.push( m );
+
+			bass[i].group.add( m );
+
+			bass_index++;
+		}
+
+	}
+
+	//	transform the keys as a whole
+	bassGroup.position.x -= HALF_WIDTH;
+
+	bassGroup.scale.x = WIDTH / bass_index;
+	bassGroup.scale.y = HEIGHT;// / 3;
+	bassGroup.scale.z = HEIGHT * .5;
 
 	// Drums
-	// drums1: Interface_1
-	// drums2: Interface_2
-	// drums3: Interface_3
-	// drums4: Interface_4
-	// drums5: Controls
-	// drums6: Tilt
+	// drums1: Interface_1 - toggle
+	// drums2: Interface_2 - button
+	// drums3: Interface_3 - button
+	// drums4: Interface_4 - button
+	// drums5: Controls - mulitslider ( 15 sliders )
+	// drums6: Tilt - tilt
+	// 
+	var drums = {
+
+		Interface_1: {
+			color: 0xF70D1B,
+			count: 1,
+			keys: [],
+			group: new THREE.Group()
+		},
+
+		Interface_2: {
+			color: 0xF71B24,
+			count: 1,
+			keys: [],
+			group: new THREE.Group()
+		},
+
+		Interface_3: {
+			color: 0xF72C32,
+			count: 1,
+			keys: [],
+			group: new THREE.Group()
+		},
+
+		Interface_4: {
+			color: 0xF73E42,
+			count: 1,
+			keys: [],
+			group: new THREE.Group()
+		},
+	}
+
+	var drumGroup = new THREE.Group();
+	group.add( drumGroup );
+
+	drum_index = 0;
+	for(var i in drums) {
+
+		drumGroup.add( drums[i].group );
+
+		for(var j=0; j<drums[i].count; j++ ) {
+
+			var m = new THREE.Mesh( boxGeometry, new LDProjectionKeyMaterial( {
+				color: drums[i].color 
+			} ) );
+
+			m.scale.x = .9;
+
+			m.position.x = drum_index + .5;
+
+			m.fadeTween = new TWEEN.Tween( m.material.uniforms.fade )
+				.to( {value: 1}, (drum_index + 1) * 50 )
+				.repeat( 100 )
+				.delay( (drum_index + 1) * 200 )
+				.yoyo( true )
+				.onUpdate( function( value ) {
+					this.scale.z = 1. + this.material.uniforms.fade.value;
+				}.bind( m ))
+
+				.start();
+
+			drums[i].keys.push( m );
+
+			drums[i].group.add( m );
+
+			drum_index++;
+		}
+
+
+	}
+
+	//	transform the keys as a whole
+	drumGroup.position.x -= HALF_WIDTH;
+	drumGroup.position.y += HEIGHT / 3;
+
+	drumGroup.scale.x = WIDTH / drum_index;
+	drumGroup.scale.y = HEIGHT * 2 / 3;
+	drumGroup.scale.z = HEIGHT * .5;
+
+	group.scale.y = -1;
+
+	rtScene.add( group );
+
 
 	// Vocals
 	// voicefx: FX
@@ -130,27 +489,112 @@ function ProjectionVisuals( options ) {
 	// pre_drums5: Trigger_5
 	// pre_drums6: Tilt
 
+	function getLineGeometry( g ) {
+
+		if( g === undefined )	g = new THREE.BufferGeometry();
+
+		var numX = Math.ceil( WIDTH / spacing );
+		var numY = Math.ceil( HEIGHT / spacing );
+
+		var numFaces = (numX + 1) * (numY + 1);
+
+		var positions = new Float32Array( numFaces * 3 * 6   );
+		var uvs = new Float32Array( numFaces * 2 * 6  );		
+
+		var squarePos = [ -.5,-.5, 0,
+						  -.5, .5, 0,
+						   .5, .5, 0,
+
+						  -.5,-.5, 0,
+						   .5, .5, 0,
+						   .5, -.5, 0
+						 ];
+
+		var spacersStep = 1;
+		if(numSpacers)	spacersStep = numX / numSpacers;
+
+		for(var i = 0, j=0, k=0, x=0, y=0, index=0; i<=numX; i++ ) {
+
+			x = i * spacing - HALF_WIDTH;
+
+			if(numSpacers && (i % spacersStep) < 1)
+			{
+				x += 100000;
+			}
+
+			for(j=0; j<=numY; j++ ) {
+
+				for( k=0; k<squarePos.length; k++ ) {
+
+					positions[index + k] = squarePos[k];
+
+				}
+
+				y = j * spacing - HALF_HEIGHT;
+
+				for( k=0; k<squarePos.length; k += 2) {
+
+					uvs[ index * 2 / 3 + k] = x;
+					uvs[ index * 2 / 3 + k + 1] = y;
+				}
+
+				index += squarePos.length;
+			}
+		}
+
+		g.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+		g.addAttribute( 'uv', new THREE.BufferAttribute( uvs, 2 ) );
+		g.computeBoundingBox();
+
+		return g;
+	}
+
 	function setup() {
 
-		var debugCube = new THREE.Mesh( new THREE.BoxGeometry( 10, 10, 10), new THREE.MeshNormalMaterial({
-			wireframe: true,
-			side: 2 
-		}) )
+		//	LINES
+		linesGeometry = getLineGeometry();
 
-		scene.add( debugCube );
+		var linesMatOptions = {
+			pMap: rt,
+			lineLength: lineLength,
+			lineWidth: lineWidth,
+			spriteRotation: spriteRotation,
+			noiseScale: noiseScale,
+			noiseAmount: noiseAmount,
+			WIDTH: WIDTH,
+			HEIGHT: HEIGHT,
+			opacity: 1
+		}
 
-		//testing
-		var m = addInsrtrument( "test", "test", { radius: 10 });
+		linesMat = new ProjectionLinesMaterial( linesMatOptions);
 
-		scene.add( m );
+		var linesMesh = new THREE.Mesh( linesGeometry, linesMat );
+		// var linesMesh1 = new THREE.Mesh( linesGeometry, new ProjectionLinesMaterial( linesMatOptions) );
+		// var linesMesh2 = new THREE.Mesh( linesGeometry, new ProjectionLinesMaterial( linesMatOptions) );
+
+		// linesMesh.material.uniforms.color.value.setRGB( 0, 0, 1 );
+		// linesMesh1.material.uniforms.color.value.setRGB( 0, 1, 0 );
+		// linesMesh2.material.uniforms.color.value.setRGB( 1, 0, 0 );
+
+		scene.add( linesMesh );
+		// scene.add( linesMesh1 );
+		// scene.add( linesMesh2 );
 
 	}
 
 	function update() {
 
+		var elapsedTime = clock.getElapsedTime();
+
+		if(linesMat)	linesMat.uniforms.time.value = elapsedTime * timeScale;
+
+		// group.rotation.y += Math.pow(Math.abs(sin( elapsedTime )), 2 ) * .01;
+
 	}
 
 	function draw() {
+
+		renderer.render( rtScene, camera, rt, true );
 
 		renderer.render( scene, camera, null, true );
 
@@ -163,6 +607,8 @@ function ProjectionVisuals( options ) {
 		update();
 
 		draw();
+
+		requestAnimationFrame(animate);
 
 	}
 
@@ -226,33 +672,19 @@ function ProjectionVisuals( options ) {
 
 	var circleGeometry = getCircleGeometry();
 
-	function getRadialInstrument ( options ) {
+	// function getRadialInstrument ( options ) {
 
-		console.log( options );
+	// 	console.log( options );
 
-		var m = new THREE.Mesh( circleGeometry, new THREE.MeshBasicMaterial( {
-			map: debugImage,
-			side: 2
-		} ) );
+	// 	var m = new THREE.Mesh( circleGeometry, new THREE.MeshBasicMaterial( {
+	// 		map: debugImage,
+	// 		side: 2
+	// 	} ) );
 
-		m.scale.multiplyScalar( options.radius || 100 );
+	// 	m.scale.multiplyScalar( options.radius || 100 );
 
-		return m;
-	}
-
-	function addInsrtrument( id, instrument_type, options ) {
-		
-		options = options || {};
-
-		switch (instrument_type) {
-
-			default:
-				instruments[ id ] = getRadialInstrument( options );
-				break;
-		}
-
-		return instruments[ id ];
-	}
+	// 	return m;
+	// }
 
 	/**
 	 * RENDERER SETUP
@@ -267,7 +699,7 @@ function ProjectionVisuals( options ) {
 
 		renderer.sortObjects = true;
 		
-		renderer.setSize( window.innerWidth, window.innerHeight );
+		renderer.setSize( WIDTH, HEIGHT );
 
 		renderer.autoClear = false;
 
@@ -296,11 +728,11 @@ function ProjectionVisuals( options ) {
 		switch(key_map[e.which])
 		{
 			case undefined:
-				console.log( e.which );	
+				console.log( e.which  + " not in key_map.");	
 				break;
 
 			default:
-				console.log(key_map[e.which] + " not in key_map.");	
+				console.log(key_map[e.which]);	
 				break;
 		}
 	}
---
+++ sketches/LDProjection/js/ProjectionLinesMaterial.js
@@ -0,0 +1,187 @@
+// ProjectionLinesMaterial.js
+// 
+// 
+console.log( "WTF" );
+var ProjectionLinesMaterial = function( params ) {
+
+	params = params || {};
+
+	var isLineShader = params.lineShader || false;
+
+	var matParams = {
+		transparent: true,
+		blending: params.blending || 1,
+		depthTest: params.depthTest || true,
+		depthWrite: params.depthWrite !== undefined ? params.depthWrite : true,
+		side: params.side || 2,// 0 = backFaceCull, 1 = frontFaceCull, 2 = doubleSided
+		linewidth: 1,
+
+		// TODO: if radius is staying at 1 lets remove it
+
+		uniforms: {
+			map: {type: 't', value: params.map },
+
+			pMap: {type: 't', value: params.pMap},
+
+			colorRamp: {type: 't', value: params.colorRamp},
+
+			color: {type: 'c', value: params.color || new THREE.Color( 1, 1, 1 ) },
+
+			opacity: {type: 'f', value: params.opacity || 1.},
+
+			lineLength: { type: 'f', value: params.lineLength || 20},
+
+			lineWidth: { type: 'f', value: params.lineWidth || 4},
+
+			time: {type: 'f', value: 0},
+
+			noiseScale: {type: 'f', value: params.noiseScale || 0.005 },
+
+			noiseAmount: {type: 'f', value: params.noiseAmount || 1 },
+
+			WIDTH: {type: 'f', value: params.WIDTH || 1 },
+
+			HEIGHT: {type: 'f', value: params.HEIGHT || 1 },
+
+			spriteRotation: {type: 'f', value: params.spriteRotation || Math.PI * 2 },
+
+			restAngle: {type: 'f', value: params.restAngle === undefined ? .75 : params.restAngle }
+
+		},
+
+		vertexShader: [
+		// 'uniform vec3 touches['+ params.touches.length+'];',
+		'uniform vec3 color;',
+		'uniform float lineLength;',
+		'uniform float lineWidth;',
+		'uniform float scale;',
+		'uniform float time;',
+		'uniform float noiseScale;',
+		'uniform float noiseAmount;',
+		'uniform float spriteRotation;',
+		'uniform float restAngle;',
+		
+		'uniform float WIDTH;',
+
+		'uniform float HEIGHT;',
+
+		'uniform sampler2D pMap;',
+
+		'uniform sampler2D colorRamp;',
+
+		'attribute vec2 positions;',
+
+		'varying vec3 vColor;',
+
+		'varying float vAlpha;',
+
+		'varying vec2 vUv;',
+
+		'# define PI ' + Math.PI,
+		'# define TWO_PI ' + (Math.PI * 2),
+
+		'float hash( float n ) { return fract(sin(n)*43758.5453123); }',
+
+		'float noise3( in vec3 x )',
+		'{',
+		'    vec3 p = floor(x);',
+		'    vec3 f = fract(x);',
+		'    f = f*f*(3.0-2.0*f);',
+		'	',
+		'    float n = p.x + p.y*157.0 + 113.0*p.z;',
+		'    return mix(mix(mix( hash(n+  0.0), hash(n+  1.0),f.x),',
+		'                   mix( hash(n+157.0), hash(n+158.0),f.x),f.y),',
+		'               mix(mix( hash(n+113.0), hash(n+114.0),f.x),',
+		'                   mix( hash(n+270.0), hash(n+271.0),f.x),f.y),f.z);',
+		'}',
+
+		'float mapLinear( in float x, in float a1, in float a2, in float b1, in float b2 ) {',
+		'	return b1 + ( x - a1 ) * ( b2 - b1 ) / ( a2 - a1 );',
+		'}',
+
+		'//rotate vector',
+		'vec3 qrot(vec4 q, vec3 v){',
+		'        return v + 2.0*cross(q.xyz, cross(q.xyz,v) + q.w*v);',
+		'}',
+
+		'//rotate vector (alternative)',
+		'vec3 qrot_2(vec4 q, vec3 v){',
+		'        return v*(q.w*q.w - dot(q.xyz,q.xyz)) + 2.0*q.xyz*dot(q.xyz,v) + 2.0*q.w*cross(q.xyz,v);',
+		'}',
+
+		'float toWeight(vec3 rgb){',
+		'	return max( rgb.x, max( rgb.y, rgb.z ) );// dot(rgb, vec3(0.299, 0.587, 0.114));',
+		'}',
+
+		'void main() {',
+
+		'	vec2 fUv = ( uv / vec2( WIDTH, HEIGHT ) ) + .5;',
+
+		'	vColor = texture2D( pMap, fUv ).xyz;', //vec3( d  );//
+
+		'	float d = max( vColor.x, max( vColor.y, vColor.z ));',
+
+		'	vec3 center = vec3( uv, 0.);',
+
+		'	float lineScale = min( d, 1.);',
+
+		'	vec3 pos = vec3( vec2( lineLength * lineScale, lineWidth * lineScale ) * position.xy, 0.);',
+
+		'	vUv = position.xy + .5;',
+
+
+		'	vAlpha = 1.;',
+
+		'	vec4 q;',
+		'	float angle = (d + .5) * (restAngle + spriteRotation + noiseAmount * noise3( center * noiseScale + vec3(0., time, 0.) ));',
+		'	q.x = 0.;',
+		'	q.y = 0.;',
+		'	q.z = sin(angle / 2.);',
+		'	q.w = cos(angle / 2.);',
+
+		'	center.z += d * 10.;',
+
+		'	gl_Position = projectionMatrix * modelViewMatrix * vec4( qrot_2(q, pos) + center, 1.0 );',
+
+
+		'}'].join('\n'),
+
+		fragmentShader: [
+
+		'# define PI ' + Math.PI,
+		'# define TWO_PI ' + (Math.PI * 2),
+
+		'uniform sampler2D map;',
+
+		'uniform sampler2D pMap;',
+
+		'uniform float opacity;',
+
+		'uniform vec3 color;',
+
+		'varying float vAlpha;',
+
+		'varying vec3 vColor;',
+
+		'varying vec2 vUv;',
+
+		'varying vec4 q;',
+
+		'float mapLinear( in float x, in float a1, in float a2, in float b1, in float b2 ) {',
+		'	return b1 + ( x - a1 ) * ( b2 - b1 ) / ( a2 - a1 );',
+		'}',
+
+		'void main()',
+		'{',
+
+		'	gl_FragColor = vec4( vColor * color, opacity);',
+
+		'}'
+		].join('\n')
+
+	}
+	
+	THREE.ShaderMaterial.call( this, matParams );
+}
+
+ProjectionLinesMaterial.prototype = Object.create( THREE.ShaderMaterial.prototype );
