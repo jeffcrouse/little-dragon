6f0a1008b786efc926c48c944c8df237732d16fa
Jeff Crouse <jeff@jeffish.com>
2015-07-07 19:07:57 -0400

    updated ld_start.sh

--- ld_start.sh
+++ ld_start.sh
@@ -4,17 +4,23 @@ killall fcserver-osx
 killall ledControl
 killall ldserver
 
+echo "=== LAUNCHING fcserver-osx ==="
 cd "${HOME}/Developer/little-dragon/fadecandy/"
 ./fcserver-osx showtime.json >> ${HOME}/Desktop/fcserver.log 2>&1 &
 
-cd "${HOME}/Developer/little-dragon/fadecandy/ledControl/application.macosx"
-open ledControl.app >> ${HOME}/Desktop/ledControl.log 2>&1 &
+sleep 5
 
+echo "=== LAUNCHING ledControl ==="
+cd "${HOME}/Developer/little-dragon/fadecandy/ledControl/application.macosx"
+open -g ledControl.app
+
+echo "=== LAUNCHING ldserver ==="
 cd "${HOME}/Developer/little-dragon/ldserver"
 node server.js >> ${HOME}/Desktop/server.log 2>&1 &
 
+echo "=== LAUNCHING 1_pink_cloud.als  ==="
 cd "${HOME}/Developer/little-dragon/songs/1_pink_cloud Project"
-open 1_pink_cloud.als
+open -g 1_pink_cloud.als
 
-
-
+echo "=== DONE  ==="
+sleep 5
